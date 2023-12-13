57a9a8970b15c812656db7b03539cbf402e728fb
Fildoo12 <filip.picek@gmail.com>
2023-12-13 12:06:41 +0100

    done

--- src/main/java/cz/secda1/spsmb/javaDates/DateTimeUtils.java
+++ src/main/java/cz/secda1/spsmb/javaDates/DateTimeUtils.java
@@ -1,9 +1,16 @@
 package cz.secda1.spsmb.javaDates;
 
+import com.sun.jdi.event.LocatableEvent;
+
+import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 public class DateTimeUtils {
@@ -15,7 +22,7 @@ public class DateTimeUtils {
      * @return String s naformátovaným datem
      */
     public static String formattedDate(LocalDateTime date) {
-        return null;
+        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
     }
 
     /**
@@ -25,7 +32,7 @@ public class DateTimeUtils {
      * @return
      */
     public static LocalDate parseDate(String dateString) {
-        return null;
+        return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     /**
@@ -35,7 +42,7 @@ public class DateTimeUtils {
      * @return vložené datum s časem 12:00
      */
     public static String atMoonTime(LocalDate date) {
-        return null;
+        return date.atStartOfDay().plusHours(12).format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
     }
 
     /**
@@ -46,12 +53,12 @@ public class DateTimeUtils {
      */
     public static String whatsTheDateToday(LocalDate today) {
         //dopolňte do proměnné dayOfWeek den v týdnu.
-        DayOfWeek dayOfWeek = null;
+        DayOfWeek dayOfWeek = today.getDayOfWeek();
 
         String dayNameInCzech = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault());
 
         //Sestavte message "Dnes je <dayNameInCzech>." pomocí String.format()
-        String formattedMessage = null;
+        String formattedMessage = String.format("Dnes je %s.", dayNameInCzech);
         return formattedMessage;
     }
 
@@ -65,7 +72,8 @@ public class DateTimeUtils {
      * @return String message
      */
     public static String daysToChristmas() {
-        return null;
+        LocalDate christmas = LocalDate.of(2023,12,24);
+        return  String.format("Do Vánoc zbývá %d dní.",christmas.compareTo(LocalDate.now()));
     }
 
     /**
@@ -81,7 +89,16 @@ public class DateTimeUtils {
      * @return List<LocalDate> s oběma seřazenými datumy
      */
     public static List<LocalDate> smallerFirst(LocalDate start, LocalDate end){
-        return null;
+        List<LocalDate> listWithDates = new ArrayList<>();
+        int x = start.compareTo(end);
+        if (x < 0){
+            listWithDates.add(start);
+            listWithDates.add(end);
+        } else {
+            listWithDates.add(end);
+            listWithDates.add(start);
+        }
+        return listWithDates;
     }
 
     /**
@@ -90,6 +107,13 @@ public class DateTimeUtils {
      * @return první pondělí po Vánocích (LocalDate)
      */
     public static LocalDate firstMondayAfterChristmas(){
+        LocalDate christmas = LocalDate.of(2023, 12, 24);
+        DayOfWeek day = christmas.plusDays(1).getDayOfWeek();
+        for (int i = 0; i < 7; i++) {
+            if (day.equals(DayOfWeek.MONDAY)){
+                return christmas.plusDays(1);
+            }
+        }
         return null;
     }
 
--- src/main/java/cz/secda1/spsmb/javaDates/Main.java
+++ src/main/java/cz/secda1/spsmb/javaDates/Main.java
@@ -1,10 +1,27 @@
 package cz.secda1.spsmb.javaDates;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 public class Main {
+    static DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    public static void main(String[] args) throws ParseException {
+        System.out.println(DateTimeUtils.formattedDate(LocalDateTime.of(2023, 11, 12, 23, 59)));
 
+        String dateString = "13.12.2023";
+        System.out.println(dateString);
 
+        LocalDate date = LocalDate.now();
+        System.out.println(DateTimeUtils.atMoonTime(date));
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(DateTimeUtils.whatsTheDateToday(LocalDate.now()));
+
+        System.out.println(DateTimeUtils.daysToChristmas());
+
     }
 }
